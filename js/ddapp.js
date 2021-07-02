c7e8b590b56422830f9a5405724bc5ed88c2e5ce
Chavezgabo11 <gechave2001@gmail.com>
2021-07-02 16:32:32 -0400

    Main Info

--- js/ddapp.js
+++ js/ddapp.js
@@ -1,48 +1,55 @@
+// wrap everything in an IIFE / module
+// a module is a JavaScript "pattern" - a programming convention
+// this keeps your code private - kinda like a "black box" - which is a best practice
+
 (() => {
-
-	// put variables (connections to the web page / DOM) at the top
-	// constant -> something that will never change / can't be redefined
+    //identify the nodes of interest in the DOM
 	const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
-				dropZoneContainer = document.querySelector(".puzzle-board"),
-				dragImages = document.querySelectorAll(".puzzle-image"),
-				dropZones = document.querySelectorAll(".drop-zone");
+				dropcontainer = document.querySelector(".puzzle-board"),
+				dragimages = document.querySelectorAll(".puzzle-image"),
+				dropzones = document.querySelectorAll(".drop-zone");
 
-	// functions go in the middle
-function swapImages() {
+		// functions go in the middle
+		function swapImages() {
+			// swap out the draggable thumbnail images
+			// update the backgound image of the drop zone dropcontainer
+			// 1. get the imageref attribute from the element we're clicking on
 
-	//let imageIndex = this.dataset.imageref;
-		//newImagePath = "images/dd/backGround" + imageIndex;
-		//newImagePath = `url(images/dd/backGround${imageIndex}.jpg)`;
+			// let imageIndex = this.dataset.imageref,
+			// 		// newImagePath = "images/dd/backGround" + imageIndex,
+			// 		newImagePath = `url(images/dd/backGround${imageIndex}.jpg)`;
 
-	dropcontainer.style.backGroundImage = `url(images/dd/backGround${this.dataset.imageref}.jpg)`;
+			// 2. set the backround image of the dropcontainer
+			dropcontainer.style.backgroundImage = `url(images/dd/backGround${this.dataset.imageref}.jpg)`;
+			// debugger;
+		}
 
-	//debugger;
-}
+		function startDrag() {
+			console.log('dragging ' + this.dataset.piecenum);
+		}
 
-function startDrag() {
-	console.log('dragging started' + this.dataset.piecenum);
-}
+		function draggedOver(event) {
+			event.preventDefault();
+			console.log('dragging over drop zone elements');
+		}
 
-function draggedOver(event) {
-	event.preventDefault();
-	console.log('dragging over drop zone elements')
-}
+		function dropped(event) {
+			event.preventDefault();
+			console.log('dropped on the element');
+			console.log(event.target.id);
+		}
 
-function dropped(event) {
-	event.preventDefault();
-	console.log('dropped on the element');
-	console.log(event.target.id);
-}
 
-	// event handling at the bottom
-dragImages.forEach(piece => {
-piece.addEventListener('dragstart',startDrag,false);
-})
+		// event handling at the bottom
+	dragimages.forEach(piece => {
+		piece.addEventListener('dragstart', startDrag, false);
+	});
 
-dropZones.forEach(zone => {
-	zone.addEventListener('drop', dropped, false);
-	zone.addEventListener('dragover', draggedOver, false);
-});
 
-puzzleSelectors.forEach(button => button.addEventListener("click", swapImages))
+	dropzones.forEach(zone => {
+		zone.addEventListener('drop',dropped, false);
+		zone.addEventListener('dragover',draggedOver, false);
+	});
+
+	puzzleSelectors.forEach(button => button.addEventListener("click", swapImages));
 })();
